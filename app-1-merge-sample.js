a98e5580b2c8114cce4d4b1f7b8152b309c9210e
for-git-lecture <emplam27@naver.com>
2026-06-30 15:05:16 +0900

    add merge file

---
+++ app-1-merge-sample.js
@@ -0,0 +1,8 @@
+const secondValue = 3
+const timeout = secondValue * 1000;
+const api_key = "sk-1234567890abcdef";
+
+// timeout 후에 실행되는 함수
+setTimeout(() => {
+    console.log(`${secondValue}초 후에 실행되는 함수`);
+}, timeout);
